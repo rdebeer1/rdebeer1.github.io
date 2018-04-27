e47d6aa8493bfd4d1a79bdc4a9ba632fad2f6922
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-04-26 19:27:59 -0500

    ui changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,9 +20,11 @@ class Main extends React.Component {
 
         <article id="projects" className={`${this.props.article === 'projects' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Projects</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <ul className="icons">
+            <a style={{ border: 'none', margin: '1rem' }} href="https://rdebeer1.github.io/Football-Database" target="_blank"> <li style={{ paddingRight: '.5rem' }}><a className="icon fa-trophy"></a></li> Football-Database</a>
+            <a style={{ border: 'none', margin: '1rem' }} href="#" target="_blank"> <li style={{ paddingRight: '.5rem' }}><a className="icon fa-headphones"></a></li> SocialMedium</a>
+            <a style={{border: 'none', margin: '1rem'}} href="#" target="_blank"> <li style={{paddingRight: '.5rem'}}><a className="icon fa-dollar"></a></li> TrumpChange</a>
+          </ul>
           {close}
         </article>
 
