35b05e63b58c5e5596bab140bc0058551e4b41b3
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-05-07 11:57:02 -0500

    style change to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => (
             <div className="inner">
                 <h1>Welcome.</h1>
                 <p><span style={{fontSize: '1.5rem'}}>I'm Robert DeBeer</span><br style={{display: 'inline-flex'}} />
-                    <span className="content">Full Stack Developer</span></p>
+                    <span style={{padding: '.5em'}} className="content">Full Stack Developer</span></p>
             </div>
         </div>
         <nav>
