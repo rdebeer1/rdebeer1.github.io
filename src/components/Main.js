95d2fadb7d8455e7055d11eeae4bb35c4ece3371
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-05-09 22:02:48 -0500

    nesting issues

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,8 +28,7 @@ class Main extends Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none', margin: 0, height: '100vh', overflow: 'scroll', paddingTop: '1em'}}>
           <h2 className="major">About</h2>
           <span className="image main" style={{ flex: 1, margin: 0, paddingBottom: '1em'}}><img style={{ flex: 1, maxWidth: '15rem'}} src={pic01} alt="" /></span> <br/>
-          <ul className="icons" style={{ marginBottom: '3em' }}>
-            <p style={{ flex: 1, letterSpacing: '2px' }}>
+          <ul className="icons" style={{ marginBottom: '3em', letterSpacing: '2px' }}>
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems: 'center' }}><span style={{ flex: .1, textAlign: 'center'}} className="icon fa-map-marker" /><span style={{ flex: 1 }}>Dallas native. Recently moved from San Diego to Austin.</span></li>
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems: 'center' }}><span style={{ flex: .1, textAlign: 'center'}} className="icon fa-briefcase" /><span style={{ flex: 1 }}>Left my last career to pursue a job that I could be more passionate about.</span></li>
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems:'center'}}><span style={{ flex: .1, textAlign: 'center' }} className="icon fa-barcode" /><span style={{ flex: 1 }}>Worked in distribution as both a Product Specialist and Business Development Representative.</span></li> 
@@ -38,7 +37,6 @@ class Main extends Component {
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems: 'center' }}><span style={{ flex: .1, textAlign: 'center' }} className="icon fa-code-fork" /><span style={{ flex: 1 }}>Attended Hack Reactor to increase my prophecy/skills with JavaScript and its many frameworks.</span></li> 
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems: 'center' }}><span style={{ flex: .1, textAlign: 'center' }} className="icon fa-search" /><span style={{ flex: 1 }}>Searching for new opportunities in the tech industry, preferably around the Austin or Dallas markets.</span></li> 
               <li style={{ display: 'flex', marginBottom: '.5em', alignItems: 'center' }}><span style={{ flex: .1, textAlign: 'center' }} className="icon fa-bolt" /><span style={{ flex: 1 }}>Driven, hardworking, and always forward thinking; being stagnant is not an option.</span ></li > 
-            </p>
           </ul>
           {close}
         </article>
